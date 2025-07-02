dfbb2538e6e6a5ca364d9175a13ac19aa8eb2080
paulina-pacheco-o <paulimichi05@gmail.com>
2025-07-02 15:42:04 +0200

    Buzz

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 for (let i = 0; i <= 100; i++) {
   let number = i
   console.log(number)
+
   if (number % 3 === 0 && number % 5 === 0) {
     console.log(`FizzBuzz`)
   }
+
   else if (number % 3 === 0) {
     console.log(`Fizz`)
+  }
+
+  if (number % 5 === 0) {
+    console.log(`Buzz`)
   }
 }
